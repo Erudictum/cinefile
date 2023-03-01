7c49f8d99ceed3b3c9b4e4d1e4db8794b8f0bb88
Rodrigo Ribeiro <rodrigo.gemini3@gmail.com>
2023-03-01 16:32:52 -0300

    Upload server files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,20 @@
 import { useState, useEffect } from 'react';
 import { GoSearch } from 'react-icons/go';
 import { MovieCard } from './MovieCard';
+import { MutatingDots } from 'react-loader-spinner';
 import './App.css';
 
 
-const API_KEY = import.meta.env.VITE_API_KEY_ENV
-const API_URL = `https://www.omdbapi.com?apikey=${API_KEY}`
-
-
 function App() {
-  
+
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
+  const [loadingIcon, setLoadingIcon] = useState(false);
 
   const searchMovies = async (title) => {
 
-    const response = await fetch(`${API_URL}&s=${title}`);
+    const response = await fetch(`http://localhost:3000?title=${title}`);
     const data = await response.json();
-
-    console.log(`${API_URL}&s=${title}`);
 
     const moviesList = data?.Search;
     if (moviesList?.length > 9) {
@@ -39,6 +35,17 @@ function App() {
   return (
     <>
       <div className='app'>
+        <MutatingDots
+          height="100"
+          width="100"
+          color="#566778"
+          secondaryColor='#455667'
+          radius='12.5'
+          ariaLabel="mutating-dots-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={loadingIcon}
+        />
         <h1>CineFile</h1>
         <h2>10.000+ <span>Posters</span> para Baixar!</h2>
 
@@ -58,18 +65,20 @@ function App() {
           />
         </div>
 
-        {movies?.length > 0 ? (
-          <div className='container'>
-            {movies.map((movie) => (
-              <MovieCard movies={movie} key={movie.imdbID} />
-            ))}
-          </div>
-        ) : (
-          <div className='empty'>
+        {movies?.length > 0
+          ? (
+            <div className='container'>
+              {movies.map((movie) => (
+                <MovieCard movies={movie} key={movie.imdbID} />
+              ))}
+            </div>
+          )
+          : (<div className='empty'>
             <h2>Nenhum filme encontrado...</h2>
             <h3>Tente uma combinação diferente de palavras.</h3>
           </div>
-        )}
+          )
+        }
       </div>
     </>
   );
