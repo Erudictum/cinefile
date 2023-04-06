396098ae296629b7cc0b257884cf47baf9daf515
Rodrigo Ribeiro <rodrigo.gemini3@gmail.com>
2023-04-06 17:27:58 -0300

    Fix scrollbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,10 +18,10 @@ function App() {
   const searchMovies = async (title) => {
     setVisibility("none")
     setLoadingIcon(true)
-    
+
     const response = await fetch(`https://cinefile.onrender.com?title=${title}`);
     const data = await response.json();
-    
+
     const moviesList = data?.Search;
     if (moviesList?.length > 9) {
       moviesList.length = 9;
@@ -31,7 +31,7 @@ function App() {
 
     setMovies(moviesList);
   }
-  
+
   const enterKey = (e) => {
     e.preventDefault();
     searchMovies(searchTerm);
@@ -39,10 +39,10 @@ function App() {
 
   const handleTip = () => {
     setTipActive("tip_active");
-    setTimeout(() => { setTipActive("")}, 2000)
+    setTimeout(() => { setTipActive("") }, 2000)
   }
 
-  
+
   return (
     <>
       <div className='app'>
@@ -57,7 +57,7 @@ function App() {
           wrapperClass="loading"
           visible={loadingIcon}
         />
-        <h1>CineFile</h1>
+        <h1 onClick={() => window.location.reload(false)}>CineFile</h1>
         <h2>10.000+ <span>Posters</span> para Baixar!</h2>
 
         <div className='search'>
@@ -81,7 +81,7 @@ function App() {
 
         {movies?.length > 0
           ? (
-            <div className='container' style={{display: `${visibility}`}}>
+            <div className='container' style={{ display: `${visibility}` }}>
               {movies.map((movie) => (
                 <MovieCard movies={movie} key={movie.imdbID} />
               ))}
